dba5d6bb523306604e41f5b48185f3ecefd980ec
Gerardo Martínez Romero <gerardo.martinez.romr@gmail.com>
2023-07-06 21:18:12 -0600

    Se añadió un modal a cada platillo de la sección platillos

--- src/app.js
+++ src/app.js
@@ -20,3 +20,31 @@ dialogElement.addEventListener("close", () => {
     inputName.value = "";
     inputMensaje.value = "";
 });
+
+// Dialog Platillos
+
+const launchPlatillosButtons = document.querySelectorAll(".platillos__btn");
+const dialogPlatillo = document.querySelector("#dialogPlatillos");
+const closeBtnPlatillo = document.querySelector("#closeOrderBtn");
+const orderName = document.querySelector("#orderName");
+const orderDesc = document.querySelector("#orderDescription");
+const closeBtnModal = document.querySelector("#closeOrderBtn");
+
+launchPlatillosButtons.forEach(platilloButton => {
+    platilloButton.addEventListener("click", openModal);
+});
+
+function openModal(event) {
+    dialogPlatillo.showModal();
+    const name = this.parentElement.children.item(0).children.item(0).innerText;
+    const desc = this.parentElement.children.item(0).children.item(1).innerText;
+
+    orderName.innerText = name;
+    orderDesc.innerText = desc;
+
+    setTimeout(closeModal, 3000);
+
+    function closeModal() {
+        event.target.parentElement.parentElement.children.item(0).children.item(1).innerText = "Casi está listo...";
+    }
+}
